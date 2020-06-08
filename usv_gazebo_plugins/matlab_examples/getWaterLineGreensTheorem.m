b656140ffa7cd2bdb756dc9d42f68a427a7dc263
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-06-08 15:50:21 -0400

    faster computations

--- usv_gazebo_plugins/matlab_examples/getWaterLineGreensTheorem.m
+++ usv_gazebo_plugins/matlab_examples/getWaterLineGreensTheorem.m
@@ -1,45 +1,20 @@
-function [torque,d,CoM] = getWaterLineGreensTheorem(theta, L, y_vals, z_vals, densityRatio, ballastLevel)
-if nargin < 4
-    % define the size and displacement of the boat
-    L = 0.6;%m
-    D = 0.5;%m
-    % this is for a boat of width 1
-    W = 1;%m
-    % define points to comprise the hull in the boat fram
-    N = 5000;
-    % define shape parameterr
-    n = 2;
-
-    y_vals_hull = linspace(-W/2,W/2,N);
-    z_vals_hull = D.*abs(2.*y_vals_hull./W).^n;
-
-    y_vals_deck = linspace(W/2,-W/2,N);
-    z_vals_deck = D*ones(size(y_vals_deck));
-    y_vals = [y_vals_hull y_vals_deck];
-    z_vals = [z_vals_hull z_vals_deck];
-end
-if nargin < 5
+function [torque,d,CoM] = getWaterLineGreensTheorem(theta, L, CoM, densityRatioWeightedArea, allPoints, densityRatio, ballastLevel)
+if nargin < 6
     densityRatio = 1/4;
 end
-if nargin < 6
+if nargin < 7
     ballastLevel = -Inf;
 end
-
-% find the com; Note: densityRatioWeightedArea is the integral over the
-% cross section weighted by the densityRatio for sections above the
-% ballastlevel and 1.24 for sections below the ballastLevel (1.24 is the
-% ratio of the density of solid PLA to water
-[densityRatioWeightedArea,ycom,zcom] = line_integral(y_vals,z_vals,ballastLevel,densityRatio);
-CoM = [ycom zcom];
+ycom = CoM(1);
+zcom = CoM(2);
 mass = densityRatioWeightedArea*L*1000;%Kg
 
 % define the direction of gravity
 down = [0, 0, -1];
 
 % rotate the boat about the origin to match the Gazebo simulator
-[y_vals_rotated,z_vals_rotated]=rotate(y_vals,z_vals,theta,0,0);
-
-[d,ycob,zcob] = fastWaterline(y_vals_rotated,z_vals_rotated,L,mass);
+[y_allPointsRotated,z_allPointsRotated]=rotate(allPoints(:,1),allPoints(:,2),theta,0,0);
+[d,ycob,zcob] = fastWaterline(y_allPointsRotated,z_allPointsRotated,L,mass);
 % find the torque
 rotated_com = [cosd(theta) sind(theta);...
                -sind(theta) cosd(theta)]*[ycom; zcom];
@@ -47,43 +22,11 @@ torque = cross([0,ycob-rotated_com(1),zcob-rotated_com(2)],-down*mass*9.8);%Nm
 torque = torque(1);
 end
 
+% TODO: cumulativeArea could be dropped in here once we support centroid
+% calculation
 function [d,ycob,zcob] = fastWaterline(y,z,L,mass)
-dz = diff(z);
-area_term = y(1:end-1).*dz;
-ycom_term = 0.5*y(1:end-1).^2.*dz;
-zcom_term = y(1:end-1).*z(1:end-1).*dz;
-
 area_displaced_at_eq = mass/L/1000;
-
-M = sortrows([z(1:end-1)' y(1:end-1)' area_term' ycom_term' zcom_term'],1);
-area = cumtrapz(M(:,3));
-ycom = cumtrapz(M(:,4))./area_displaced_at_eq;
-zcom = cumtrapz(M(:,5))./area_displaced_at_eq;
-eq_index = min(find(area > area_displaced_at_eq));
-
-d = M(eq_index,1);
-ycob = ycom(eq_index);
-zcob = zcom(eq_index);
-end
-
-function [ycob,zcob,area] = cob(y,z,n,theta,d,da,D,W)
-% find the cob
-aboveHullGrid = aboveHull(y,z,n,D,W,theta);
-belowWaterGrid = belowWater(y,z,d);
-belowDeckGrid = belowDeck(y,z,n,D,W,theta);
-index = find(aboveHullGrid & belowWaterGrid & belowDeckGrid);
-area = length(index).*da;
-ycob = sum(y(index).*da)./area;
-zcob = sum(z(index).*da)./area;
-end
-
-function aboveHullGrid = aboveHull(y,z,n,D,W,theta)
-% the hull function
-% transform y,z into boat frame by rotating by -theta
-yBoatFrame = y*cosd(-theta) + z*sind(-theta);
-zBoatFrame = y*sind(theta) + z*cosd(-theta);
-hullBoatFrame = D.*abs(2.*yBoatFrame/W).^n;
-aboveHullGrid = zBoatFrame > hullBoatFrame;
+[d,ycob,zcob] = cumulativeArea([y z],area_displaced_at_eq);
 end
 
 % Define a rotation about a point (ycom,zcom)
